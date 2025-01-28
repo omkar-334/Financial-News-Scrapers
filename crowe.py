56ff8b58d92f08699ba66269f9002288d6b256e5
Omkar Kabde <omkarkabde@gmail.com>
2025-01-28 21:44:40 +0530

    TODO: crowe

---
+++ crowe.py
@@ -0,0 +1,3 @@
+api_url = "https://www.crowe.com/api/sitecore/FilteredGrid/GetGrid?id=%7BCBFDB8B3-5F73-4EC6-99FC-55A3EAFA175A%7D&languageName=en-US&page=2&isDateDesc=true&enforceNormal=True"
+
+"https://www.crowe.com/insights"
